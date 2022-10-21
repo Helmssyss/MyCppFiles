20e02057b064607dc0f48545892214c4a24d48ee
Arif <84701901+Arif-Helmsys@users.noreply.github.com>
2022-10-21 20:40:03 +0300

    Update CreditCardAlgorithm.cpp

--- CreditCardAlgorithm.cpp
+++ CreditCardAlgorithm.cpp
@@ -1,19 +1,19 @@
-﻿#include <iostream>
+#include <iostream>
 #include <locale.h>
 using namespace std;
 
-#define CC_COUT 16
+#define CC_COUNT 16
 
 bool creditCardState() {
 	int cc_nums[CC_COUT]{4,4,2,6,7,1,0,0,3,5,8,1,6,1,8,2};
 	int count = 0;
-	for(int i = 0; i < CC_COUT; i+=2){
+	for(int i = 0; i < CC_COUNT; i+=2){
 		cc_nums[i] = cc_nums[i] * 2;
 		if (cc_nums[i] > 9){
 			cc_nums[i] = cc_nums[i] % 10 + cc_nums[i] / 10;
 		}
 	}
-	for(int j = 0; j < CC_COUT; j ++){
+	for(int j = 0; j < CC_COUNT; j ++){
 		count += cc_nums[j];
 	}
 	if (count % 10 == 0) {
