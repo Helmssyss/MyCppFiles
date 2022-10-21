aa74e876388244bed043e16511010afa5f430b0a
Arif <84701901+Arif-Helmsys@users.noreply.github.com>
2022-10-21 21:29:08 +0300

    Add files via upload

---
+++ HistogramProgram.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <locale.h>
+#include <fmt/core.h>
+
+using namespace std;
+
+void histogram(int*);
+void banner();
+
+int main() {
+	banner();
+}
+
+void banner() {
+	setlocale(LC_ALL, "Turkish");
+	const int arr_size = 6;
+
+	cout << fmt::format("Dizi Boyutu {}\n\n",arr_size);
+	cout << "Indis \t Dizi Elemanı \t Histogram\n";
+
+	int myArr[arr_size]{};
+	histogram(myArr);
+}
+
+void histogram(int* arr) {
+	srand((unsigned int)time(0));
+	for(int i = 0; i < 6; i++){
+		arr[i] = rand() % 30;
+		cout << fmt::format(" {0}\t  {1}\t\t ", i, arr[i]);
+		for(int j = 0; j < arr[i]; j++){
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+}
